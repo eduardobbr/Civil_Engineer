3e6463d5fced35fcfc3331d78344a0339d877efe
eduardo.rocha <eduardomj.bryan@gmail.com>
2024-01-19 12:02:59 -0300

    alterado tudo de nav bar etc

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,19 +8,22 @@
     <title>Civil Enginner</title>
 
     <!-- links -->
-    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/dashboard.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
         integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
-    
+
     <header>
         <div class="navbar">
             <div class="logo"><a href="#">Civil Enginner</a></div>
             <ul class="links">
                 <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact</a></li>
             </ul>
             <a href="#" class="action_btn">Get Started</a>
             <div class="toggle_btn">
@@ -30,6 +33,9 @@
 
         <div class="dropdown_menu">
             <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact</a></li>
             <li><a href="#" class="action_btn">Get Started</a></li>
         </div>
     </header>
