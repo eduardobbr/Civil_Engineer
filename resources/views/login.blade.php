cedee488b49eaa22c5d44cec1120b36e830dcf6b
eduardo.rocha <eduardomj.bryan@gmail.com>
2024-01-23 08:59:03 -0300

    rota realizada do dashboard para o login e algumas alterçacoes necessarias

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,33 +8,45 @@
     <title>Civil Enginner</title>
 
     <!-- links -->
-    <link rel="stylesheet" href="css/login.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/dashboard.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
         integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 </head>
 
 <body>
 
     <header>
         <div class="navbar">
-            <div class="logo"><a href="#">Civil Enginner</a></div>
+            <div class="logo"><a href="{{ route('dashboard')}}"><img src="./img/logo.png" alt=""></div>
             <ul class="links">
-                <li><a href="#">Home</a></li>
+                <li><a href="{{ route('dashboard')}}">Home</a></li>
             </ul>
-            <a href="#" class="action_btn">Get Started</a>
             <div class="toggle_btn">
                 <i class="fa-solid fa-bars"></i>
             </div>
         </div>
 
         <div class="dropdown_menu">
-            <li><a href="#">Home</a></li>
-            <li><a href="#" class="action_btn">Get Started</a></li>
+            <li><a href="{{ route('dashboard')}}">Home</a></li>
         </div>
     </header>
 
-    <main></main>
+    <main>
+        <section class="home_section" id="home">
+            <h2>Bem-vindo a Civil Engineer</h2>
+            <p>Seja nosso cliente e utilize o melhor sistema para controle de obras existente do mercado!</p>
+        </section>
+    </main>
+
+
+    <footer>
+        <p class="rodape-direitos">&copy;2023 - Todos os Direitos Reservados.</p>
+    </footer>
+
 
     <script>
         const toggleBtn = document.querySelector('.toggle_btn')
@@ -45,11 +57,16 @@
             dropDownMenu.classList.toggle('open')
             const isOpen = dropDownMenu.classList.contains('open')
 
-            toggleBtnIcon.classList = isOpen 
-            ? 'fa-solid fa-xmark'
-            : 'fa-solid fa-bars'
+            toggleBtnIcon.classList = isOpen ?
+                'fa-solid fa-xmark' :
+                'fa-solid fa-bars'
         }
     </script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
+
 
 </body>
 
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
+
+// Rota para a pagina principal 
 
 Route::get('/', function () {
     return view('dashboard');
-});
+})->name('dashboard');
+
+
+
+
+// Rotas para o login
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
